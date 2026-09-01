3cd0fd361b52ff0edf4c1d949ce21115b0e1c09b
ffxwrld <213770836+ffxwrld@users.noreply.github.com>
2026-09-01 12:21:52 +0200

    update

--- src/utils/webrtc.ts
+++ src/utils/webrtc.ts
@@ -19,7 +19,13 @@ export class WebRTCManager {
 
   constructor(private callbacks: PeerConnectionCallbacks) {
     this.pc = new RTCPeerConnection({
-      iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
+      iceServers: [
+        { urls: 'stun:stun.l.google.com:19302' },
+        { urls: 'stun:global.stun.twilio.com:3478' },
+        { urls: 'turn:openrelay.metered.ca:80', username: 'openrelayproject', credential: 'openrelayproject' },
+        { urls: 'turn:openrelay.metered.ca:443', username: 'openrelayproject', credential: 'openrelayproject' },
+        { urls: 'turn:openrelay.metered.ca:443?transport=tcp', username: 'openrelayproject', credential: 'openrelayproject' }
+      ]
     });
 
     this.pc.onicecandidate = (event) => {
